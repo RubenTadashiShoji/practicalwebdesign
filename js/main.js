c2e4fe1e5ee9053b7f905265e9f22cf91d785711
Ruben Epong <ruben.epong@tadashishoji.com>
2019-04-20 15:14:52 -0700

    started instafeed

--- js/main.js
+++ js/main.js
@@ -1,15 +1,26 @@
-$(document).ready(function() {
-	
-	$('.hamburger-menu').click(function() {
+$(document).ready(function () {
+
+	$('.hamburger-menu').click(function () {
 		event.stopPropagation();
 		if ($('main').hasClass('active')) {
-			$('main').removeClass('active');		
+			$('main').removeClass('active');
 		} else {
-			$('main').addClass('active');		
-		}		
+			$('main').addClass('active');
+		}
 	});
-	
-	$('main').click(function() {
+
+	$('main').click(function () {
 		$('main').removeClass('active');
 	})
+
+
+	$('#blog').waypoint(function (direction) {
+		if (direction == 'down') {
+			$('header').addClass('sticky');
+		} else {
+			$('header').removeClass('sticky');
+		}
+	});
+
+
 });
